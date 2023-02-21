f1112bbb66d152eb87b4439968c163b7aec37b1f
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-21 23:36:54 +0530

    added userController.js

---
+++ controllers/userController.js
@@ -0,0 +1,27 @@
+const User = require("../models/userModel");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({ status: "success", data: { users } });
+});
+exports.createUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: "error", message: "This route is not yet defined" });
+};
+exports.getUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: "error", message: "This route is not yet defined" });
+};
+exports.updateUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: "error", message: "This route is not yet defined" });
+};
+exports.deleteUser = (req, res) => {
+  res
+    .status(500)
+    .json({ status: "error", message: "This route is not yet defined" });
+};
