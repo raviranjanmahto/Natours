a2a58fa86ea860484442e7d1a1d8a188d3ee9a80
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-22 22:30:29 +0530

    added delete curret user logic

--- controllers/userController.js
+++ controllers/userController.js
@@ -23,6 +23,11 @@ exports.updateMe = catchAsync(async (req, res, next) => {
   res.status(200).json({ status: "success", data: { user } });
 });
 
+exports.deleteMe = catchAsync(async (req, res, next) => {
+  await User.findByIdAndUpdate(req.user.id, { active: false });
+  res.status(204).json({ status: "success", data: null });
+});
+
 exports.createUser = (req, res) => {
   res
     .status(500)
