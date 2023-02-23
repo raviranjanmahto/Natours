d2ce9ce27a439fb26d0865e9974442730ce29f00
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-23 22:33:49 +0530

    added review model

---
+++ models/reviewModel.js
@@ -0,0 +1,51 @@
+const mongoose = require("mongoose");
+
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "Review can not be empty!"],
+    },
+    rating: {
+      type: Number,
+      required: true,
+      min: [1, "A rating must be above 1.0"],
+      max: [5, "A rating must be below 5.0"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    tour: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Tour",
+      required: [true, "Review must belong to a tour."],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "Review must belong to a user"],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+reviewSchema.pre(/^find/, function (next) {
+  //   this.populate({
+  //     path: "tour",
+  //     select: "name",
+  //   }).populate({
+  //     path: "user",
+  //     select: "name photo",
+  //   });
+  this.populate({
+    path: "user",
+    select: "name photo",
+  });
+  next();
+});
+
+const Review = mongoose.model("Review", reviewSchema);
+module.exports = Review;
