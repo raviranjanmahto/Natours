e67dd15ecdad5e1a59f1e019bc9b6da15a155314
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-23 22:46:23 +0530

    added review routes n getting tour based on review populate

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const authController = require("../controllers/authController");
+const reviewController = require("../controllers/reviewController");
+
+const router = express.Router({ mergeParams: true });
+
+router
+  .route("/")
+  .get(authController.protect, reviewController.getAllReview)
+  .post(
+    authController.protect,
+    authController.restrictTo("user"),
+    reviewController.createReview
+  );
+
+module.exports = router;
