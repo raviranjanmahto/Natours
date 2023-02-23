656983b15556a25cd64a74bcbcba638b9974ca3c
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-23 22:49:14 +0530

    using express merge params for routes

--- dev-data/data/import-dev-data.js
+++ dev-data/data/import-dev-data.js
@@ -12,9 +12,7 @@ mongoose
   .catch(err => console.log("Error connecting to database🙄", err));
 
 //   READ JSON FILE
-const tours = JSON.parse(
-  fs.readFileSync(`${__dirname}/tours-simple.json`, "utf-8")
-);
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/tours.json`, "utf-8"));
 
 // IMPORT DATA INTO DATABASE
 const importData = async () => {
