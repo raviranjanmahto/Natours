fdc1fb31c252d0ead83fcedb27f9c8dab5217f5c
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-02-25 23:33:25 +0530

    getting tours within radius

--- models/tourModel.js
+++ models/tourModel.js
@@ -41,6 +41,7 @@ const tourSchema = new mongoose.Schema(
       default: 4.5,
       min: [1, "Rating must be above 1.0"],
       max: [5, "Rating must be below 5.0"],
+      set: val => Math.round(val * 100) / 100, //4.666666, 466.666, 467, 4.67
     },
     ratingQuantity: {
       type: Number,
@@ -126,6 +127,7 @@ const tourSchema = new mongoose.Schema(
 // Compound INDEX
 tourSchema.index({ price: 1, ratingAverage: -1 });
 tourSchema.index({ slug: 1 });
+tourSchema.index({ startLocation: "2dsphere" });
 
 tourSchema.virtual("durationWeeks").get(function () {
   return this.duration / 7;
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -20,6 +20,10 @@ router
   );
 
 router
+  .route("/tours-within/:distance/center/:latlng/unit/:unit")
+  .get(tourController.getToursWithin);
+
+router
   .route("/")
   .get(tourController.getAllTours)
   .post(
