4add5c4ad76d520144f161d73868626830cfb8eb
raviranjan mahto <rvrnjnmahto@gmail.com>
2023-09-28 19:33:30 +0530

    account page added

--- routes/viewRoutes.js
+++ routes/viewRoutes.js
@@ -4,10 +4,12 @@ const authController = require("../controllers/authController");
 
 const router = express.Router();
 
+router.get("/me", authController.protect, viewController.getAccount);
+
 router.use(authController.isLoggedIn);
 
 router.get("/", viewController.getOverview);
 router.get("/tour/:slug", viewController.getTour);
-router.get("/login/", viewController.getLoginForm);
+router.get("/login", viewController.getLoginForm);
 
 module.exports = router;
